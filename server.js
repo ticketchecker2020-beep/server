25b6036108f6982eed3c702269cf8b4ebe4e1726
ticketchecker2020-beep <ticketchecker2020@gmail.com>
2026-07-12 19:09:00 +0300

    Fix CSP: allow inline event handlers in dashboard

--- server.js
+++ server.js
@@ -516,6 +516,7 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
       imgSrc: ["'self'", "https:", "data:"],
